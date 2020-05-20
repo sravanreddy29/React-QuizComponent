ed6146b77c977526037fa6d3d60a852d5123b4ef
Sravan Chilumula (IN4536) <M1044536@mindtree.com>
2020-05-20 19:21:41 +0530

    quiz reset  added

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,6 +15,10 @@ class Quiz extends Component {
     this.setState({ quiz_position: this.state.quiz_position +1})
   }
 
+  handleResetClick(){
+    this.setState({ quiz_position: 1})
+  }
+
   render() {
     const isQuizEnd =
       ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
@@ -22,7 +26,8 @@ class Quiz extends Component {
     return (
       <div className={"QuizQuestion"}>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd 
+          resetClickHandler={this.handleResetClick.bind(this)}/>
         ) : (
           <QuizQuestion
             quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
